f3b9a23656aa752be58ac63ead012fe5d54ce16e
Jonathan Bisson <research@bjonnh.net>
2020-12-03 18:59:34 -0600

    [Feature] Add a tool to detect duplicate DOIs and start a list

---
+++ src/main/kotlin/net/nprod/lotus/tools/duplicateDOIdetector/main.kt
@@ -0,0 +1,33 @@
+package net.nprod.lotus.tools.duplicateDOIdetector
+
+import net.nprod.lotus.helpers.GZIPReader
+import net.nprod.lotus.helpers.parseTSVFile
+import net.nprod.lotus.wdimport.wd.query.WDKT
+import java.io.File
+
+fun main() {
+    val fileName = "data/tmp/split_aa"
+    val fileReader = try {
+        GZIPReader(fileName).bufferedReader
+    } catch (e: java.util.zip.ZipException) {
+        File(fileName).bufferedReader()
+    }
+    val file = parseTSVFile(fileReader)
+
+    fileReader.close()
+
+    val dois = mutableSetOf<String>()
+
+    file?.map {
+        val doi = it.getString("referenceCleanedDoi")
+        dois.add(doi)
+    }
+    println("We have ${dois.size} unique DOIs")
+    println("We will now search for those with duplicates")
+    val wdkt = WDKT()
+    val counts = dois.map {
+        it to wdkt.searchDOI(it).query.searchinfo.totalhits
+    }.filter { it.second > 1 }
+    println("Here is the list of duplicates")
+    println(counts)
+}
