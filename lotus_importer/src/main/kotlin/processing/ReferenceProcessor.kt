e9fcd05c6c8ae25ca87541d4fe1bd1810cb6e4c6
Jonathan Bisson <research@bjonnh.net>
2020-12-30 22:07:18 -0600

    [Feature] Now check the articles on CrossREF to make sure we have all the info on them.

--- lotus_importer/src/main/kotlin/processing/ReferenceProcessor.kt
+++ lotus_importer/src/main/kotlin/processing/ReferenceProcessor.kt
@@ -9,7 +9,9 @@ package processing
 
 import input.DataTotal
 import input.Reference
+import io.ktor.util.KtorExperimentalAPI
 import net.nprod.lotus.wdimport.wd.InstanceItems
+import net.nprod.lotus.wdimport.wd.MainInstanceItems
 import net.nprod.lotus.wdimport.wd.WDFinder
 import net.nprod.lotus.wdimport.wd.models.entries.WDArticle
 import net.nprod.lotus.wdimport.wd.publishing.IPublisher
@@ -24,17 +26,22 @@ class ReferenceProcessor(
     private val logger = LogManager.getLogger(ReferenceProcessor::class.qualifiedName)
     private val articlesCache: MutableMap<Reference, WDArticle> = mutableMapOf()
 
+    @KtorExperimentalAPI
     private fun articleFromReference(reference: Reference): WDArticle {
         val article = WDArticle(
             label = reference.title ?: reference.doi,
             title = reference.title,
-            doi = reference.doi.toUpperCase(), // DOIs are always uppercase
+            doi = reference.doi.toUpperCase(), // DOIs are always uppercased but in reality we see both
         ).tryToFind(wdFinder, instanceItems)
-
+        article.populateFromCrossREF(wdFinder, instanceItems)
         publisher.publish(article, "upserting article")
         return article
     }
 
+    /**
+     * Generate a WikiData article from that reference
+     */
+    @KtorExperimentalAPI
     fun get(key: Reference): WDArticle {
         return articlesCache.getOrPut(key) {
             articleFromReference(key)
