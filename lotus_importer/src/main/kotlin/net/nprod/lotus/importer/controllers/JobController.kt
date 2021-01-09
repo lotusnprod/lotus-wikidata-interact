5e196496749922098291aecc4e035bdb3763e37f
Jonathan Bisson <bjonnh-code@bjonnh.net>
2021-01-08 19:10:31 -0600

    [Dev] Got the inputReader almost ready to take the files

--- lotus_importer/src/main/kotlin/net/nprod/lotus/importer/controllers/JobController.kt
+++ lotus_importer/src/main/kotlin/net/nprod/lotus/importer/controllers/JobController.kt
@@ -10,13 +10,14 @@ package net.nprod.lotus.importer.controllers
 import kotlinx.serialization.Serializable
 import net.nprod.lotus.importer.jobs.ImportJob
 import org.springframework.batch.core.BatchStatus
+import org.springframework.batch.core.Job
 import org.springframework.batch.core.JobExecution
 import org.springframework.batch.core.JobParametersBuilder
 import org.springframework.batch.core.launch.JobLauncher
 import org.springframework.beans.factory.annotation.Qualifier
-import org.springframework.context.ApplicationContext
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.ResponseBody
 
 
@@ -42,29 +43,31 @@ data class JobData(
 
 @Controller
 class JobController(
-    val importJob: ImportJob, val applicationContext: ApplicationContext,
-    @Qualifier("asyncJobLauncher") val jobLauncher: JobLauncher
+    val importJob: ImportJob,
+    @Qualifier("asyncJobLauncher") val jobLauncher: JobLauncher,
+    @Qualifier("newJob") val job: Job
 ) {
     private var lastJob: JobExecution? = null
 
     @GetMapping("/jobs/import/request", produces = ["application/json"])
     @ResponseBody
-    fun newRunjob(): JobData? {
+    fun newRunjob(
+        @RequestParam(name = "max_records") maxRecords: Long? = null,
+        @RequestParam(name = "skip") skip: Long? = null
+    ): JobData? {
         if (lastJob == null || lastJob?.status in arrayOf(
                 BatchStatus.COMPLETED,
                 BatchStatus.FAILED,
                 BatchStatus.STOPPED
             )
         ) {
+            val parameters = JobParametersBuilder().addLong("unique", System.nanoTime())
+            maxRecords?.let { parameters.addLong("max_records", it) }
+            skip?.let { if (it >= 0) parameters.addLong("skip", it) }
             try {
-                lastJob = jobLauncher.run(
-                    importJob.newJob(),
-                    JobParametersBuilder().addLong("unique", System.nanoTime()).toJobParameters()
-                )
-
+                lastJob = jobLauncher.run(job, parameters.toJobParameters())
             } catch (e: Exception) {
                 e.printStackTrace()
-                return null
             }
         }
         return lastJob?.let { JobData.fromJobExecution(it) }
--- lotus_importer/src/main/kotlin/net/nprod/lotus/importer/jobs/ImportJob.kt
+++ lotus_importer/src/main/kotlin/net/nprod/lotus/importer/jobs/ImportJob.kt
@@ -8,26 +8,25 @@
 package net.nprod.lotus.importer.jobs
 
 import org.springframework.batch.core.Job
+import org.springframework.batch.core.JobParameters
 import org.springframework.batch.core.Step
+import org.springframework.batch.core.StepExecution
+import org.springframework.batch.core.annotation.BeforeStep
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory
-import org.springframework.batch.core.launch.support.RunIdIncrementer
 import org.springframework.batch.item.ItemProcessor
 import org.springframework.batch.item.ItemReader
 import org.springframework.batch.item.ItemWriter
-import org.springframework.batch.item.ParseException
-import org.springframework.batch.item.UnexpectedInputException
 import org.springframework.batch.item.file.FlatFileItemReader
 import org.springframework.batch.item.file.mapping.DefaultLineMapper
 import org.springframework.batch.item.file.mapping.FieldSetMapper
 import org.springframework.batch.item.file.transform.DelimitedLineTokenizer
 import org.springframework.batch.item.file.transform.FieldSet
-import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.io.FileSystemResource
 import org.springframework.core.io.Resource
-import java.net.MalformedURLException
 
 
 data class Transaction(
@@ -40,59 +39,73 @@ class RecordFieldSetMapper : FieldSetMapper<Transaction?> {
     }
 }
 
-class CustomItemProcessor : ItemProcessor<Transaction, Transaction> {
-    override fun process(item: Transaction): Transaction = item
-}
-
 @Configuration
 class ImportJob(val jobs: JobBuilderFactory, val steps: StepBuilderFactory) {
+    private var maxCount: Long? = null
+
     @Value("file:data/buggy.tsv")
     lateinit var inputFile: Resource
 
-    @Value("file:data/outtest.tsv")
-    lateinit var outputFile: Resource
+    @Bean
+    fun itemReader(): FlatFileItemReader<Transaction> = MyReader()
 
     @Bean
-    fun itemReader(): ItemReader<Transaction> {
-        val reader: FlatFileItemReader<Transaction> = FlatFileItemReader<Transaction>()
+    fun itemProcessor(): ItemProcessor<Transaction, Transaction> = CustomItemProcessor()
+
+    @Bean
+    fun itemWriter(): ItemWriter<Transaction> = MyWriter()
+
+    @Bean
+    protected fun step1(itemReader: ItemReader<Transaction>): Step {
+        return steps["step1"].chunk<Transaction, Transaction>(10)
+            .reader(itemReader).processor(itemProcessor()).writer(itemWriter()).build()
+    }
+
+    @Bean(name = ["newJob"])
+    fun newJob(step1: Step): Job {
+        return jobs["importJob"].start(step1).build()
+    }
+}
+
+class MyReader : FlatFileItemReader<Transaction>() {
+    init {
         val tokenizer = DelimitedLineTokenizer("\t")
         val tokens = arrayOf("database")
         tokenizer.setNames(*tokens)
         tokenizer.setStrict(false)
-        reader.setResource(inputFile)
+        setResource(FileSystemResource("data/buggy.tsv"))
         val lineMapper: DefaultLineMapper<Transaction> = DefaultLineMapper<Transaction>()
         lineMapper.setLineTokenizer(tokenizer)
         lineMapper.setFieldSetMapper(RecordFieldSetMapper())
-        reader.setLineMapper(lineMapper)
-        return reader
+        setLineMapper(lineMapper)
+        setLinesToSkip(1) // Header
     }
 
-    @Bean
-    fun itemProcessor(): ItemProcessor<Transaction, Transaction> {
-        return CustomItemProcessor()
+    @BeforeStep
+    fun beforeStep(stepExecution: StepExecution) {
+        stepExecution.jobParameters.getLong("max_records")?.let { setMaxItemCount(it.toInt()) }
+        stepExecution.jobParameters.getLong("skip")?.let { setLinesToSkip(it.toInt()+1) }
     }
 
-    @Bean
-    @Throws(MalformedURLException::class)
-    fun itemWriter(): ItemWriter<Transaction> {
-        val itemWriter = MyWriter()
-
-        return itemWriter
-    }
-
-    protected fun step1(): Step {
-        return steps["step1"].chunk<Transaction, Transaction>(10)
-            .reader(itemReader()).processor(itemProcessor()).writer(itemWriter()).build()
-    }
-
-    @Bean(name = ["newJob"])
-    fun newJob(): Job {
-        return jobs["firstBatchJob"].start(step1()).build()
-    }
 }
 
 class MyWriter : ItemWriter<Transaction> {
     override fun write(items: MutableList<out Transaction>) {
         println("Trying to write $items")
+    }
+}
+
+class CustomItemProcessor : ItemProcessor<Transaction, Transaction> {
+    private var parameters: JobParameters? = null
+
+    @BeforeStep
+    fun beforeStep(stepExecution: StepExecution) {
+        parameters = stepExecution.jobParameters
+    }
+
+
+    override fun process(item: Transaction): Transaction {
+        println("unique is $parameters")
+        return item
     }
 }
