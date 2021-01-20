a1ae20bc40a76fd65e3ec4f3ffa09143ffa124d6
Jonathan Bisson <bjonnh-code@bjonnh.net>
2021-01-20 12:59:27 -0600

    [Bug] Retry when serializing fails.

--- wdkt/src/main/kotlin/net/nprod/lotus/wdimport/wd/query/WDKT.kt
+++ wdkt/src/main/kotlin/net/nprod/lotus/wdimport/wd/query/WDKT.kt
@@ -13,14 +13,20 @@ import io.ktor.client.engine.cio.endpoint
 import io.ktor.client.request.get
 import io.ktor.client.request.parameter
 import kotlinx.coroutines.runBlocking
+import kotlinx.serialization.SerializationException
+import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
+import net.nprod.lotus.helpers.tryCount
 import net.nprod.lotus.wdimport.wd.MainInstanceItems
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
 import org.wikidata.wdtk.datamodel.interfaces.PropertyIdValue
 
 /**
  * A way to run queries directly using WDTK
  */
 class WDKT : IWDKT {
+    private val logger: Logger = LogManager.getLogger(WDKT::class)
     private val client: HttpClient = HttpClient(CIO) {
         engine {
             endpoint {
@@ -37,14 +43,25 @@ class WDKT : IWDKT {
     override fun searchDOI(doi: String): QueryActionResponse = searchForPropertyValue(MainInstanceItems.doi, doi)
 
     override fun searchForPropertyValue(property: PropertyIdValue, value: String): QueryActionResponse {
-        val out: String = runBlocking {
-            client.get("https://www.wikidata.org/w/api.php") {
-                parameter("action", "query")
-                parameter("format", "json")
-                parameter("list", "search")
-                parameter("srsearch", "haswbstatement:\"${property.id}=$value\"")
+        val out: String =
+            runBlocking {
+                client.get("https://www.wikidata.org/w/api.php") {
+                    parameter("action", "query")
+                    parameter("format", "json")
+                    parameter("list", "search")
+                    parameter("srsearch", "haswbstatement:\"${property.id}=$value\"")
+                }
             }
+
+        return tryCount(
+            listOf(SerializationException::class),
+            maxRetries = 10,
+            delayMilliSeconds = 10_000L,
+            logger = logger
+        ) {
+            Json.decodeFromString(out)
         }
-        return Json.decodeFromString(QueryActionResponse.serializer(), out)
     }
+
+    class JsonDecodingException(override val message: String) : Exception()
 }
