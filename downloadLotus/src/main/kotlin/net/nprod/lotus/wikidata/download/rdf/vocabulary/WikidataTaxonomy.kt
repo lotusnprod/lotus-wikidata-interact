829d76d34289781ef17e51b1fa6c567d8a1bc15f
Adriano Rutz <44283913+Adafede@users.noreply.github.com>
2021-12-28 19:13:13 +0100

    Update WikidataTaxonomy.kt

--- downloadLotus/src/main/kotlin/net/nprod/lotus/wikidata/download/rdf/vocabulary/WikidataTaxonomy.kt
+++ downloadLotus/src/main/kotlin/net/nprod/lotus/wikidata/download/rdf/vocabulary/WikidataTaxonomy.kt
@@ -11,9 +11,12 @@ object WikidataTaxonomy {
         val taxonName = Wikidata.wdt("P225")
         val taxonRank = Wikidata.wdt("P105")
         val parentTaxon = Wikidata.wdt("P171")
+        val algaeId = Wikidata.wdt("P1348")
         val irmngId = Wikidata.wdt("P5055")
         val gbifId = Wikidata.wdt("P846")
         val ncbiId = Wikidata.wdt("P685")
+        val otlId  = Wikidata.wdt("P9157")
+        val wfoId  = Wikidata.wdt("P7715")
         val wormsId = Wikidata.wdt("P850")
         val parentTaxonChain = "<" + Wikidata.p("P171") + ">/<" + Wikidata.ps("P171") + ">"
     }
