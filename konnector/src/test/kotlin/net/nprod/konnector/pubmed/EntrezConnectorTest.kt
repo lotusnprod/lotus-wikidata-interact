f7f74a7c6a09081f04b9a6d594eadf0ed4847b96
Adriano Rutz <adriano.rutz@hotmail.com>
2025-10-04 13:57:56 +0200

    up

--- konnector/src/test/kotlin/net/nprod/konnector/pubmed/EntrezConnectorTest.kt
+++ konnector/src/test/kotlin/net/nprod/konnector/pubmed/EntrezConnectorTest.kt
@@ -10,7 +10,7 @@ import kotlin.time.ExperimentalTime
 @ExperimentalTime
 internal class EntrezConnectorTest {
     private val eFetchPubmedParser = EFetchPubmedParser()
-    private var eSearchConn: EntrezConnector = EntrezConnector(System.getenv("NCBI_APIKEY"), 1500)
+    private var eSearchConn: EntrezConnector = EntrezConnector(System.getenv("NCBI_APIKEY"), 2500)
 
     @Test
     fun `basic search`() {
